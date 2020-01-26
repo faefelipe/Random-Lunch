bd9c941a39e229185f856ec40c7d2dc4f20808c9
faefelipe <fae.20@hotmail.com>
2020-01-26 01:52:05 -0300

    Initial tests to project

--- App.js
+++ App.js
@@ -1,12 +1,91 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import {
+  Button,
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  SectionList,
+} from 'react-native';
 
-export default class HelloWorldApp extends Component {
+
+const DATA = [
+  {
+    title: 'Main dishes',
+    data: ['Pizza', 'Burger', 'Risotto'],
+  },
+  {
+    title: 'Sides',
+    data: ['French Fries', 'Onion Rings', 'Fried Shrimps'],
+  },
+  {
+    title: 'Drinks',
+    data: ['Water', 'Coke', 'Beer'],
+  },
+  {
+    title: 'Desserts',
+    data: ['Cheese Cake', 'Ice Cream'],
+  },
+];
+
+
+function Item({ title }) {
+  
+  return (
+    <View style={styles.item}>
+      <Text style={styles.title}>{title}</Text>      
+      <Button
+        onPress={this._onPressButton}
+        title="Press Me"
+      />
+    </View>  
+  );
+}
+
+export default class App extends Component {  
+  _onPressButton() {
+    alert('You tapped the button!')
+  }
+
   render() {
     return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text>Hello, world!</Text>
-      </View>
+      <SafeAreaView style={styles.container}>
+        <SectionList
+          sections={DATA}
+          keyExtractor={(item, index) => item + index}
+          renderItem={({ item }) => <Item title={item} />}
+          renderSectionHeader={({ section: { title } }) => (
+            <Text style={styles.header}>{title}{Button}</Text>
+          )}
+        />
+      </SafeAreaView>
     );
-  }
+  }  
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 20,
+    marginHorizontal: 16,
+  },
+  item: {
+    backgroundColor: '#f9c2ff',
+    padding: 20,
+    marginVertical: 8,
+  },
+  header: {
+    fontSize: 32,
+  },
+  title: {
+    fontSize: 24,
+  },
+  alternativeLayoutButtonContainer: {
+    margin: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  buttonContainer: {
+    margin: 20
+  },
+});
---
+++ readme.md
@@ -0,0 +1,19 @@
+## Processo de Codificação de um Aplicativo Mobile, com a finalidade de sortear um restaurante 
+## para que um grupo de pessoas possa decidir aonde almoçar. Contando Geolocalização, Restaurantes Preferidos, Não Repetição de restaurante em uma semana, Necessidade de utilizar Uber/99Pop, 
+
+## Todo List
+
+# Tela carregamento - Preload
+# Permissão de Geolocalização
+# Carregamento de API MAPS PLACES
+# Apresentar Listagem dos Restaurantes Disponiveis 
+# Botão Sortear Restaurante 
+# Apresentar Restaurante Sorteado
+# Adicionar filtros (Uber, Sexta-feira = "dia do Lixo")
+
+## Stack 
+
+# React Native
+# Node 
+# Google Places 
+# Google Maps 
